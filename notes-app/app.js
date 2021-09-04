7eb76ac2d36d6347c0355b2b2b48a269f82524e1
Leo Gonzalez <leoggonzalez@hotmail.com>
2021-09-04 10:16:02 +0200

    test third party package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,10 @@
+const validator = require('validator');
 const { getNotes } = require('./notes.js');
 
 const notes = getNotes();
 
 console.log(notes);
+
+console.log(validator.isURL('http://asd.com', {
+  require_protocol: true
+}));
