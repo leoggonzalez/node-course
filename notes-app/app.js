71203dda6440da8021ed783c42e7b3d98913091d
Leo Gonzalez <leoggonzalez@hotmail.com>
2021-09-04 10:02:06 +0200

    removed example code

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,5 @@
-const { name, add } = require('./utils.js');
 const { getNotes } = require('./notes.js');
 
-const sum = add(4, 2);
 const notes = getNotes();
 
-console.log(name);
-console.log(sum);
 console.log(notes);
