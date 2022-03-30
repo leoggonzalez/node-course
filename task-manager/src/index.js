53f90939b66fa44981bd7d1ae23a25a6dd91c01c
Leo Gonzalez <leoggonzalez@hotmail.com>
2022-03-30 22:35:37 +0200

    added jwt

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,8 +7,11 @@ const app = express();
 
 const port = process.env.PORT || '3000';
 
-app.use(express.json());
+app.use((req, res, next) => {
 
+});
+
+app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
