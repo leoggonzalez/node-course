15c76da521db08dcc89026143159dcbbee9511e3
Leo Gonzalez <leoggonzalez@hotmail.com>
2022-06-21 23:08:30 +0200

    added tasks relationship

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -3,6 +3,7 @@ require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 const { Task } = require('./model/task');
+const { User } = require('./model/user');
 
 const app = express();
 
@@ -20,4 +21,8 @@ app.listen(port, () => {
   // const task = await Task.findById('626ae3eb81e8584cdc672ee8');
   // await task.populate('owner');
   // console.log(task);
+
+  const user = await User.findById('626ae30474a1a457a503dd76');
+  await user.populate('tasks');
+  console.log(user.tasks);
 })();
